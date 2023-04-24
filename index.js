f6e554d0194407a1e0f284c52b4c8a91d0c38d1d
Carlmelo Booc <car1melo@hotmail.com>
2023-04-24 17:49:40 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,18 +1,46 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  let answer = []
+  for( i = 0; i < array.length ; i++){
+    for( j = i+1; j<array.length; j++){
+      if(array[i]+array[j]===target){
+        answer.push(array[i], array[j])
+        return true
 }
+    }
+  }
+  return false
+}
+
+  // Write your algorithm here
+
+
 
 /* 
   Write the Big O time complexity of your function here
+
+the first method is the brute force method and the Big-O notation of this solution would be O(*2) for time complexity and O(1) for space complexity.
+
+
 */
 
 /* 
   Add your pseudocode here
 */
 
+
+
 /*
   Add written explanation of your solution here
+
+1. Write a function called hasTargetSum that receives two arguments:
+2. an array of integers
+3. a target integer
+4. The function should return true if any pair of numbers in the array adds up to the target number.
+
+  
 */
+
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
